bad778e35bc2e97d7a0f26a534e9de0e35105ada
ozan_ozgur <ozan1.ozgur@gmail.com>
2019-11-24 00:34:39 +0000

    added refactored functions and tests for 1st requirement.

--- run_phone_mast_analysis_report.py
+++ run_phone_mast_analysis_report.py
@@ -2,7 +2,10 @@
 # -*- coding: utf-8 -*-
 
 import argparse
+import csv
 import logging
+import os
+from tabulate import tabulate
 
 
 logging.basicConfig(
@@ -13,26 +16,67 @@ logging.basicConfig(
 logger = logging.getLogger(__name__)
 
 
-def read_input_csv_file():
-    pass
+def check_file_exists(csv_file):
+    """Throws exception if the given file path does not exist.
+
+    :param csv_file: str
+    :return:
+    """
+    if not os.path.isfile(os.path.abspath(csv_file)):
+        logger.error(f'Input file {csv_file} not found!')
+        raise ValueError
+    return None
 
 
-def sort_data_by_current_rent(ascending=True):
-    pass
+def read_and_sort_csv_data_by_current_rent(csv_file, descending=False):
+    """Reads the given csv file and returns the data sorted by the 'Current Rent' column
+
+    :param csv_file:    str
+    :param descending:  bool
+    :return: list of dicts
+    """
+    with open(csv_file, 'r') as fh:
+        logger.info(f'Reading data from {csv_file} ...')
+        csv_reader = csv.DictReader(fh)
+        sorted_data = sorted(csv_reader, key=lambda row: float(row['Current Rent']), reverse=descending)
+    return sorted_data
 
 
-def get_top_n_items_from_list_data(num_of_items):
-    pass
+def get_top_n_items_from_list_data(data_set, num_of_items):
+    """Gets the first n number of elements from a list
+
+    :param data_set:        list of dicts
+    :param num_of_items:    int
+    :return: dict
+    """
+    top_dataset = data_set[:num_of_items]
+    # creating a list of lists in order to tabulate this data ...
+    table_list = [[row['Property Name'], row['Unit Name'], row['Tenant Name'], row['Current Rent']] for row in
+                  top_dataset]
+    return {
+        'table': table_list,
+        'headers': ('Property Name', 'Unit Name', 'Tenant Name', 'Current Rent')
+    }
+
+
+def print_tabulated_output(list_obj, headers):
+    """Produces a tabulated output of the from the given data set
+
+    :param list_obj:    list of lists, where each sublist represent a row of data
+    :param headers:     list of column headers
+    :return:
+    """
+    print(tabulate(list_obj, headers=headers))
 
 
 def arg_parser():
     parser = argparse.ArgumentParser(description='This script produces some analysis on mobile '
                                                  'phone mast data provided as csv input.')
     # positional arg
-    parser.add_argument('csvfile', action='store', help='file path of csv')
+    parser.add_argument('csv_file', action='store', help='file path of csv')
 
     # optional arg
-    parser.add_argument('-r', '--rent', action='store', default=5, type=int,
+    parser.add_argument('-r', action='store', dest='top_rents', type=int,
                         help='prints top n number of records of data sorted in ascending order '
                              'by current rent')
     args = parser.parse_args()
@@ -41,6 +85,17 @@ def arg_parser():
 
 def main():
     cmdline_args = arg_parser()
+    print('\n')
+
+    check_file_exists(cmdline_args.csv_file)
+    if cmdline_args.top_rents:
+        sorted_rent_data = read_and_sort_csv_data_by_current_rent(cmdline_args.csv_file)
+        top_rents = get_top_n_items_from_list_data(data_set=sorted_rent_data, num_of_items=cmdline_args.top_rents)
+        print(f'\n>> Top {cmdline_args.top_rents} mobile phone mast sites in ascending order of current rent ...\n')
+        print_tabulated_output(list_obj=top_rents['table'], headers=top_rents['headers'])
+
+    print('\n')
+    logger.info('--end--')
 
 
 if __name__ == '__main__':
--- test_run_phone_mast_analysis_report.py
+++ test_run_phone_mast_analysis_report.py
@@ -1,18 +1,72 @@
+from argparse import Namespace
+from collections import OrderedDict
+import mock
 import pytest
+import sys
 
 import run_phone_mast_analysis_report as analysis_report
 
 
+@pytest.fixture()
+def sample_csv_file(tmpdir):
+    """ Fixture creates a CSV source file with data.
+
+    Returns the csv file path.
+    """
+    csv_file = tmpdir.mkdir('sub').join('sample_phone_masts.csv')
+    # header and 3 rows of test data
+    csv_file.write(
+        'Property Name,Property Address,Unit Name,Tenant Name,Lease Start Date,Lease End Date,Lease Years,'
+        'Current Rent\n'
+        'Farmhouse 2,Field X,Unit 2,CellWorks Ltd,29 Apr 2008,28 Apr 2018,10,700,\n'
+        'Farmhouse 1,Field Y,Unit 1,CellWorks Ltd,29 Apr 2002,28 Apr 2020,18,500,\n'
+        'Farmhouse 3,Field Z,Unit 3,CellWorks Ltd,01 Dec 2019,01 Dec 2021,2,999.99,\n'
+        )
+    return str(csv_file)
+
+
 class TestPhoneMastAnalysisReport(object):
 
-    def test_read_input_csv_file(self):
-        pass
+    def test_check_file_exists(self, sample_csv_file):
+        file_exists = analysis_report.check_file_exists(sample_csv_file)
+        assert file_exists is None
 
-    def test_sort_data_by_current_rent(self):
-        pass
+    def test_check_file_exists_fails(self, sample_csv_file):
+        with pytest.raises(ValueError):
+            analysis_report.check_file_exists('./invalid_non_existent_file_foobar.csv')
 
-    def test_get_top_n_items_from_list_data(self):
-        pass
+    def test_read_and_sort_csv_data_by_current_rent(self, sample_csv_file):
+        sorted_data = analysis_report.read_and_sort_csv_data_by_current_rent(sample_csv_file)
+        assert len(sorted_data) == 3
+        assert isinstance(sorted_data[0], OrderedDict)
+        assert sorted_data[0]['Current Rent'] == '500'
+        assert sorted_data[1]['Current Rent'] == '700'
+        assert sorted_data[2]['Current Rent'] == '999.99'
 
-    def test_arg_parser(self):
-        pass
+    def test_get_top_n_items_from_list_data(self, sample_csv_file):
+        sorted_data = analysis_report.read_and_sort_csv_data_by_current_rent(sample_csv_file)
+        result = analysis_report.get_top_n_items_from_list_data(data_set=sorted_data, num_of_items=1)
+        expected_outcome = {
+            'table': [['Farmhouse 1',  'Unit 1', 'CellWorks Ltd', '500']],
+            'headers': ('Property Name', 'Unit Name', 'Tenant Name', 'Current Rent')
+        }
+        assert expected_outcome == result
+
+    def test_print_tabulated_output(self, capsys):
+        expected_input = {
+            'table': [['Farmhouse 1', 'Unit 1', 'CellWorks Ltd', '500']],
+            'headers': ('Property Name', 'Unit Name', 'Tenant Name', 'Current Rent')
+        }
+        analysis_report.print_tabulated_output(list_obj=expected_input['table'], headers=expected_input['headers'])
+        std_out, _ = capsys.readouterr()
+        assert std_out == ('Property Name    Unit Name    Tenant Name      Current Rent\n'
+                           '---------------  -----------  -------------  --------------\n'
+                           'Farmhouse 1      Unit 1       CellWorks Ltd             500\n')
+
+    @mock.patch('sys.argv')
+    def test_arg_parser(self, m_sys_argv, sample_csv_file, ):
+        sys.argv = ['program.py', sample_csv_file, '-r', '5']
+
+        expected_args = Namespace(csv_file=sample_csv_file, top_rents=5)
+        args = analysis_report.arg_parser()
+        assert args == expected_args
